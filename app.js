c4cc63a2e526a8b116d4e53a57032562cf2cf125
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2021-07-26 01:14:08 +0530

    Js begining

---
+++ app.js
@@ -0,0 +1,11 @@
+var  para = document.querySelector("#headerpara");
+var  txtamount = document.querySelector("#txtamount");
+var  nextbtn = document.querySelector("#next-btn");
+var  txtcash = document.querySelector("#txtcash");
+var  lasttb =document.querySelectorAll("#lasttb")
+
+nextbtn.addEventListener("click", clickfunction);
+ 
+function clickfunction(){
+console.log("clickkk");
+}
