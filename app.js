2f82ba8aadbc0213d49f5fed1b85d3e4d4ccfca6
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2021-07-28 02:25:38 +0530

    feat main Functionality

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ var  para = document.querySelector("#headerpara");
 var  txtamount = document.querySelector("#txtamount");
 var  nextbtn = document.querySelector("#next-btn");
 var  txtcash = document.querySelector("#txtcash");
-var  lasttb =document.querySelectorAll("#lasttb")
+var  lasttb =document.querySelectorAll(".lasttb");
+var  currencyArray = [2000,500,200,100,50,20,10,5,2,1];
+let returnArray =[0,0,0,0,0,0,0]
 
 nextbtn.addEventListener("click", clickfunction);
  
 function clickfunction(){
-console.log("clickkk");
+ var result = txtcash.value-txtamount.value;
+ 
+
+ 
+ if(result<0){
+     return "Invalid Amount";
+ } else if(result>0){
+     var remainingamount = result;
+    for(var i=0;i<currencyArray.length;i++){
+     returnArray[i]= Math.floor(remainingamount/currencyArray[i]);
+     remainingamount-=returnArray[i]*currencyArray[i];
+     lasttb[i].innerText=returnArray[i];
+
+ }
 }
+}
+ 
+ 
+    
