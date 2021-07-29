4629fd6bbaf7d2d76ac86794c82b9e8d7425a680
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2021-07-29 11:41:15 +0530

    Feature: Basic Feature
    Basic feature of the app. All features in next commit.

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
+
+
+
+
 var  para = document.querySelector("#headerpara");
+var  mainsection = document.querySelector("#main-container")
+var  billcontainer = document.querySelector("#billcontainer");
 var  txtamount = document.querySelector("#txtamount");
 var  nextbtn = document.querySelector("#next-btn");
+var  cashcontainer = document.querySelector("#cashcontainer");
 var  txtcash = document.querySelector("#txtcash");
-var  lasttb =document.querySelectorAll(".lasttb");
+var  secondarycontainer = document.querySelector("#secondary-container");
+var  maintable = document.querySelector("#maintable");
+var  lasttb = document.querySelectorAll(".lasttb");
 var  currencyArray = [2000,500,200,100,50,20,10,5,2,1];
-let returnArray =[0,0,0,0,0,0,0]
+let returnArray =[0,0,0,0,0,0,0];
+
+secondarycontainer.style.visibility="hidden";
+cashcontainer.style.visibility="hidden";
+
+
+
 
 nextbtn.addEventListener("click", clickfunction);
- 
-function clickfunction(){
- var result = txtcash.value-txtamount.value;
- 
+// txtcash.style.visibility="hidden";
 
  
+function clickfunction(){
+var result = txtcash.value-txtamount.value;
+console.log(txtcash.value);
+  
  if(result<0){
      return "Invalid Amount";
  } else if(result>0){
@@ -24,7 +40,10 @@ function clickfunction(){
 
  }
 }
+
+
+
 }
- 
- 
+
+
     
